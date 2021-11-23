de2113b2f3d16520d830676df017edf31b8b796a
valeriaachirus <90177725+valeriaachirus@users.noreply.github.com>
2021-11-23 16:30:04 +0200

    Create Patient.java

---
+++ Patient.java
@@ -0,0 +1,18 @@
+package javaapplication6;
+
+import java.util.Date;
+
+public class Patient extends Person
+{
+    public String id;
+    public Date accepted;
+    public String[] sickness;
+    public String[] prescriptions;
+    public String[] allergies;
+    public String[] specialReqs;
+
+    public int age()
+    {
+        return accepted.getYear() - birthDate.getYear();
+    }
+}
