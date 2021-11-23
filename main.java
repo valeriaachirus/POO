6622979ab1295c044fbacad7d2da8215840e779b
valeriaachirus <90177725+valeriaachirus@users.noreply.github.com>
2021-11-23 16:34:33 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+package javaapplication6;
+
+
+import javaapplication6.Organization;
+
+public class main {
+    static public void main(String[] args)
+    {
+        Organization org = new Organization();
+
+        System.out.println(org);
+    }
+}
