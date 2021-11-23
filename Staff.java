21a5f08f99ba0a7c9622452d994c194c70f401c1
valeriaachirus <90177725+valeriaachirus@users.noreply.github.com>
2021-11-23 16:31:11 +0200

    Create Staff.java

---
+++ Staff.java
@@ -0,0 +1,11 @@
+package javaapplication6;
+
+import java.util.Date;
+
+public class Staff extends Person
+{
+    public Date joined;
+    public String[] education;
+    public String[] certifications;
+    public String[] languages;
+}
