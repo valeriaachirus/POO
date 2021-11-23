3208832d0081e1862d0ece05b1b2767a59ed222a
valeriaachirus <90177725+valeriaachirus@users.noreply.github.com>
2021-11-23 16:20:35 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+package javaapplication5;
+
+public class main
+{
+    static public void main(String[] args)
+    {
+        J j = new J("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", new X("X"));
+
+        j.printClass();
+    }
+}
