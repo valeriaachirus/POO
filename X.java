6b7edc0fbf07ab54d6d265e09e91ae17ba4a0d29
valeriaachirus <90177725+valeriaachirus@users.noreply.github.com>
2021-11-23 16:20:14 +0200

    Create X.java

---
+++ X.java
@@ -0,0 +1,17 @@
+package javaapplication5;
+
+public class X  {
+
+    private String name;
+
+    public X(String x)
+    {
+        name = x;
+    }
+
+    public String getX()
+    {
+        return name;
+    }
+
+}
